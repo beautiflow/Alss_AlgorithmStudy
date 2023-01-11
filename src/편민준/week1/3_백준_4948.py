8405803cc3929616ce6fc2074dd74d409addbeb8
PyeonMinjun <alswns9810@gamil.com>
2023-01-11 14:59:08 +0900

    편민준 : 3일차

---
+++ src/편민준/week1/3_백준_4948.py
@@ -0,0 +1,20 @@
+def d(n):
+    isprime = [True] * (2*n +1)
+    sn = int(2* n ** 0.5)
+
+    for i in range(2,sn+1):
+        if isprime:
+            for j in range(i*2, 2*n+1, i):
+                isprime[j] = False
+    return [x for x in range(n+1, (2*n)+1) if isprime[x]]
+
+
+while True:
+    a = int(input())
+    if a == 0:
+        break
+    print(len(d(a)))
+
+   
+    
+
