562699a6ad84b5b0f564af4f5e9fed5fd7e943c0
PyeonMinjun <alswns9810@gamil.com>
2023-01-12 13:46:39 +0900

    백준1

--- src/편민준/week1/3_백준_4948.py
+++ src/편민준/week1/3_백준_4948.py
@@ -1,6 +1,6 @@
 def d(n):
     isprime = [True] * (2*n +1)
-    sn = int((2* n )** 0.5)
+    sn = int(2* n ** 0.5)
 
     for i in range(2,sn+1):
         if isprime:
@@ -14,7 +14,6 @@ while True:
     if a == 0:
         break
     print(len(d(a)))
-###########
 
    
     
