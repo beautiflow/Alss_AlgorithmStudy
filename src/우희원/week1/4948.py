1e370e4904d5db351db4b0f74d955ba52eb76521
H2W0N <woohw97@naver.com>
2023-01-12 11:59:34 +0900

    우희원 : 백준 4948 베르트랑 공준

--- src/우희원/week1/4948.py
+++ src/우희원/week1/4948.py
@@ -2,16 +2,47 @@ def is_prime(n) :
     if n == 1 :
         return False
     else :
-        for i in range(2, int(n/2)+1) :
+        for i in range(2, int(n**0.5)+1) :
             if n%i == 0 :
                 return False
         return True
 
-n = int(input())
-result = 0
-if n == 0 :
-    pass
-for i in range(n, 2*n+1) :
-    if is_prime(i) == True :
-        result += 1
-print(result)
+value = list(range(123456*2))
+prime = list()
+
+for i in value :
+    if is_prime(i) :
+        prime.append(i)
+
+while 1 :
+    n = int(input())
+    cnt = 0
+    if n == 0 :
+        break
+    for i in prime :
+        if n < i <= 2*n :
+            cnt += 1
+    print(cnt)
+
+
+
+
+# 시간초과
+# def is_prime(n) :
+#     if n == 1 :
+#         return False
+#     else :
+#         for i in range(2, int(n**0.5)+1) :
+#             if n%i == 0 :
+#                 return False
+#         return True
+    
+# while True :
+#     n = int(input())
+#     cnt = 0
+#     if n == 0 :
+#         break
+#     for i in range(n+1, 2*n+1) :
+#         if is_prime(i) == True :
+#             cnt += 1
+#     print(cnt)
