4335d776814f0158660f829b42f9f11723802e30
NaYeon Oh <105831341+beautiflow@users.noreply.github.com>
2023-01-16 00:25:52 +0900

    Nayeon (#54)
    * 오나연: 백준 10974
    
    * 오나연: 백준 10974 설명 추가
    
    * 오나연: 백준 1929
    
    * 오나연: 백준 9012
    
    * merge (#52)
    
    * 오나연: 백준 1929 (#47)
    
    * 오나연: 백준 10974
    
    * 오나연: 백준 10974 설명 추가
    
    * 오나연: 백준 1929
    
    * Minjun (#48)
    
    * atrox
    
    * 편민준 : 3일차
    
    * 편민준 : 3일차
    
    * 편민준 : 4일
    
    * 4_백준_9020
    
    * test
    
    * 백준_day1
    
    * 백준1
    
    * 0112
    
    * 편민준 : Fri
    
    * 편민준 : day2
    
    Co-authored-by: PyeonMinjun <alswns9810@gamil.com>
    
    * nayeon (#49)
    
    * 오나연: 백준 10974
    
    * 오나연: 백준 10974 설명 추가
    
    * 오나연: 백준 1929
    
    * nayeon (#50)
    
    * 오나연: 백준 10974
    
    * 오나연: 백준 10974 설명 추가
    
    * 오나연: 백준 1929
    
    * Nayeon (#51)
    
    * 오나연: 백준 10974
    
    * 오나연: 백준 10974 설명 추가
    
    * 오나연: 백준 1929
    
    Co-authored-by: PyeonMinjun <60938276+PyeonMinjun@users.noreply.github.com>
    Co-authored-by: PyeonMinjun <alswns9810@gamil.com>
    
    * 오나연: 백준 1978
    
    * main -> nayeon merge (#53)
    
    * 오나연: 백준 1929 (#47)
    
    * 오나연: 백준 10974
    
    * 오나연: 백준 10974 설명 추가
    
    * 오나연: 백준 1929
    
    * Minjun (#48)
    
    * atrox
    
    * 편민준 : 3일차
    
    * 편민준 : 3일차
    
    * 편민준 : 4일
    
    * 4_백준_9020
    
    * test
    
    * 백준_day1
    
    * 백준1
    
    * 0112
    
    * 편민준 : Fri
    
    * 편민준 : day2
    
    Co-authored-by: PyeonMinjun <alswns9810@gamil.com>
    
    * nayeon (#49)
    
    * 오나연: 백준 10974
    
    * 오나연: 백준 10974 설명 추가
    
    * 오나연: 백준 1929
    
    * nayeon (#50)
    
    * 오나연: 백준 10974
    
    * 오나연: 백준 10974 설명 추가
    
    * 오나연: 백준 1929
    
    * Nayeon (#51)
    
    * 오나연: 백준 10974
    
    * 오나연: 백준 10974 설명 추가
    
    * 오나연: 백준 1929
    
    Co-authored-by: PyeonMinjun <60938276+PyeonMinjun@users.noreply.github.com>
    Co-authored-by: PyeonMinjun <alswns9810@gamil.com>
    
    Co-authored-by: PyeonMinjun <60938276+PyeonMinjun@users.noreply.github.com>
    Co-authored-by: PyeonMinjun <alswns9810@gamil.com>

---
+++ src/오나연/week2/B_9012.java
@@ -0,0 +1,50 @@
+package 오나연.week2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.EventListener;
+import java.util.Stack;
+
+public class B_9012 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++){
+            sb.append(solve(br.readLine())).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    public static String solve(String s){
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+
+            char c = s.charAt(i);
+
+            if(c == '('){
+                stack.push(c);
+            }
+            else if (stack.empty()) {
+                return "NO";
+            }
+            else {
+                stack.pop();
+            }
+        }
+
+        if(stack.empty()){
+            return "YES";
+        }
+        else {
+            return "NO";
+        }
+    }
+}
