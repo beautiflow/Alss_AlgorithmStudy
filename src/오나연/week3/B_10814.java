498ac8dce9e56aefff2efa51ddc9a010276ce01e
beautiflow <1114ny@naver.com>
2023-01-27 08:07:59 +0900

    오나연: 백준 10814

--- src/오나연/week3/B_10814.java
+++ src/오나연/week3/B_10814.java
@@ -1,24 +1,36 @@
 package 오나연.week3;
 
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.StringTokenizer;
-//
-//public class B_10814 {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        int N = Integer.parseInt((br.readLine()));
-//
-//        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-//
-//        int A = Integer.parseInt(st.nextToken());
-//        String S = st.nextToken();
-//
-//        for(int i = 0; i < N ; i++){
-//
-//        }
-//
-//
-//    }
-//}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class B_10814 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt((br.readLine()));
+        String[][] arr = new String[N][2];
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] age1, String[] age2) {
+                return Integer.parseInt(age1[0]) - Integer.parseInt(age2[0]);
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++){
+            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
