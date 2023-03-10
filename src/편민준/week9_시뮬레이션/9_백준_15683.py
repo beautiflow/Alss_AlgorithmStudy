330157d9a8c6a3d1951952b87f3acf4363214882
PyeonMinjun <alswns9810@gmail.com>
2023-03-10 15:38:37 +0900

    편민준 : 0313

---
+++ src/편민준/week9_시뮬레이션/9_백준_15683.py
@@ -0,0 +1,31 @@
+# import sys
+from collections import deque
+# input = sys.stdin.readline
+
+# bfs 탐색
+def bfs(v):
+    queue = deque([v])
+    cnt = 0
+    # 사람의 수만큼 반복하여 지목한 사람을 확인
+    for _ in range(n):
+        target = queue.popleft()
+        print("t",target)
+        cnt += 1 
+
+        # 지목한 사람이 보성이의 번호이면 지목 횟수를 리턴
+        if graph[target] == k:
+            return cnt
+
+        # 지목한 번호를 다시 큐에 추가하여 반복
+        queue.append(graph[target])
+        print("q",queue)
+
+    # 사람 수만큼 지목이 끝난 후에도 보성이의 번호가 아니면 -1 리턴
+    return -1
+
+
+n, k = map(int, input().split())
+graph = [int(input()) for _ in range(n)]
+
+# 0번부터 지목
+print(bfs(0))
